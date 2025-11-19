ae163b26b44ed85ba4a08b98ea7bb73b21feb368
Caleb Brock <calebbrock19@gmail.com>
2025-11-19 17:03:09 +0800

    made changes to the comments so i can understnad what i did

--- src/HttpServer.java
+++ src/HttpServer.java
@@ -1,8 +1,19 @@
-public class Main {
-    public static void main(String[] args){
+// wont be doing https (no encryption)
 
-        
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class HttpServer{
+    public static void main(String[] args) throws IOException {
+
+        int port = 63342; // giving a port value for now to test
+        ServerSocket socket = new ServerSocket(port); // listening to port
+
+
+        // constantly waiting for a request to be made on port number.
+        while(true){
 
         }
 
     }
+}
