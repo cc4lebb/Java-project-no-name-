a77269f4ad4031159626b70e9a4ccfb961252c64
Caleb Brock <calebbrock19@gmail.com>
2025-11-21 16:10:07 +0800

    testing commit

--- src/HttpServer.java
+++ src/HttpServer.java
@@ -1,19 +1,29 @@
 // wont be doing https (no encryption)
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class HttpServer{
     public static void main(String[] args) throws IOException {
 
         int port = 63342; // giving a port value for now to test
-        ServerSocket socket = new ServerSocket(port); // listening to port
+        ServerSocket serverSocket = new ServerSocket(port); // listening to port
 
+            // constantly waiting for a request to be made on port number.
+            while(true) {
+                Socket clientSocket = serverSocket.accept();
 
-        // constantly waiting for a request to be made on port number.
-        while(true){
+                // output to client
+                PrintWriter output = new PrintWriter(clientSocket.getInputStream(), true);
 
-        }
+                // input from client
+                BufferedReader input = new BufferedReader
+                        (new InputStreamReader(clientSocket.getInputStream()));
 
+            }
     }
 }
