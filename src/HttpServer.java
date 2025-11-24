5e0e290261eb05372faf5e176137bd82a2f8aaa5
Caleb Brock <calebbrock19@gmail.com>
2025-11-24 23:13:18 +0800

    changed PrintWriter to OutputStream instead / better for bytes

--- src/HttpServer.java
+++ src/HttpServer.java
@@ -1,30 +1,34 @@
 // wont be doing https (no encryption)
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class HttpServer{
     public static void main(String[] args) throws IOException {
+        int port = 8080; // giving a port value for now to test
 
-        int port = 63342; // giving a port value for now to test
         ServerSocket serverSocket = new ServerSocket(port); // listening to port
+        System.out.println("listening on port" + port);
 
             // constantly waiting for a request to be made on port number.
             while(true) {
+                Socket clientSocket = serverSocket.accept(); // connect to port
 
-                Socket clientSocket = serverSocket.accept();
+                // variables for output and input
+                BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // input from client
+                OutputStream output = clientSocket.getOutputStream(); // output to client
 
-                // output to client
-                PrintWriter output = new PrintWriter(clientSocket.getInputStream(), true);
+                FileReader file = new FileReader("index.html");
 
-                // input from client
-                BufferedReader input = new BufferedReader
-                        (new InputStreamReader(clientSocket.getInputStream()));
+                // if file is read
 
+                //else 404 not found
+
+                // close client socket after the header is printed
+                clientSocket.close();
+                file.close();
             }
+
     }
 }
