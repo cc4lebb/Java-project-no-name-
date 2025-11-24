24955fc7721801fec7a4c4a39a0d32095949f23c
Caleb Brock <calebbrock19@gmail.com>
2025-11-25 01:02:52 +0800

    handle client method

--- src/HttpServer.java
+++ src/HttpServer.java
@@ -1,5 +1,3 @@
-// wont be doing https (no encryption)
-
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -9,26 +7,24 @@ public class HttpServer{
         int port = 8080; // giving a port value for now to test
 
         ServerSocket serverSocket = new ServerSocket(port); // listening to port
-        System.out.println("listening on port" + port);
+        System.out.println("listening on port " + port);
+        System.out.println("go to browser/ command line and run:\nhttp://localhost:8080");
 
             // constantly waiting for a request to be made on port number.
             while(true) {
                 Socket clientSocket = serverSocket.accept(); // connect to port
 
-                // variables for output and input
-                BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // input from client
-                OutputStream output = clientSocket.getOutputStream(); // output to client
-
-                FileReader file = new FileReader("index.html");
-
-                // if file is read
-
-                //else 404 not found
-
-                // close client socket after the header is printed
-                clientSocket.close();
-                file.close();
+                handleClient(clientSocket);
             }
+    }
 
+    private static void handleClient(Socket clientSocket){
+        // read the request and send it back
+
+        // variables for output and input
+        BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // input from client
+        OutputStream output = clientSocket.getOutputStream(); // output to client
+
+        // read the incoming HTTP request: GET request
     }
 }
